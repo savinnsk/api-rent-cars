f62dd46942b983b3fe190a3dae5b9a4b7eacd9bc
Savio Picanço <saviopbrito@outlook.com>
2022-10-29 15:05:39 -0300

    feat: add signupController

---
+++ src/presentation/controllers/signup-controller.ts
@@ -0,0 +1,19 @@
+import { MissingParamsError } from "../errors/missing-params-error"
+import { badRequest, ok } from "../helpers/http.helper"
+import { Controller } from "../protocols/controllers"
+import { HttpRequest, HttpResponse } from "../protocols/http"
+
+export class SignupController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['name', "username", 'email', 'password', 'driver_license']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
+      }
+    }
+    const account = await httpRequest.body
+
+    return ok(account)
+  }
+}
