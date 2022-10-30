03746f41d53e39098d7220ac96235b2824c5c49e
Savio Picanço <saviopbrito@outlook.com>
2022-10-30 15:55:33 -0300

    should 400 if an invalid email is provided

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,6 +1,8 @@
+import { InvalidParamsError } from "../errors/invalid-params-error"
 import { MissingParamsError } from "../errors/missing-params-error"
 import { badRequest } from "../helpers/http.helper"
 import { EmailValidator } from "../protocols/email-validator-protocols"
+// import { HttpRequest } from "../protocols/http-protocols"
 import { SignupController } from "./signup-controller"
 
 interface sutTypes {
@@ -11,20 +13,29 @@ interface sutTypes {
 const makeEmailValidatorStub = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
-      return false
+      return true
     }
   }
 
   return new EmailValidatorStub()
 }
+/* const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: "valid_name",
+    username: "valid_username",
+    password: "valid_password",
+    email: "valid_mail@mail.com",
+    driver_license: "valid_driver_license"
+  }
+}) */
 
 const makeSut = (): sutTypes => {
   const emailValidatorStub = makeEmailValidatorStub()
   const sut = new SignupController(emailValidatorStub)
 
   return {
-    emailValidatorStub,
-    sut
+    sut,
+    emailValidatorStub
   }
 }
 
@@ -95,5 +106,24 @@ describe("signupController", () => {
     const httpResponse = await sut.handle(HttpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MissingParamsError("driver_license")))
+  })
+
+  test("should 400 if an invalid email is provided", async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        username: "valid_username",
+        password: "valid_password",
+        email: "invalid_mail@mail.com",
+        driver_license: "valid_driver_license"
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new InvalidParamsError("email")))
   })
 })
--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,3 +1,4 @@
+import { InvalidParamsError } from "../errors/invalid-params-error"
 import { MissingParamsError } from "../errors/missing-params-error"
 import { badRequest, ok, serverError } from "../helpers/http.helper"
 import { Controller } from "../protocols/controllers-protocols"
@@ -19,8 +20,14 @@ export class SignupController implements Controller {
           return badRequest(new MissingParamsError(field))
         }
       }
-      const { email, ...account } = await httpRequest.body
-      this.emailValidator.isValid(email)
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const { name, username, email, password, driver_license } = await httpRequest.body
+      const account = { name, username, email, password, driver_license }
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamsError("email"))
+      }
 
       return ok(account)
     } catch (error) {
