0272380e4429f15875f92b784f98c4826c80adb5
Savio Picanço <saviopbrito@outlook.com>
2022-10-30 11:34:04 -0300

    refactor: add try cacth to signupController

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,19 +1,23 @@
 import { MissingParamsError } from "../errors/missing-params-error"
-import { badRequest, ok } from "../helpers/http.helper"
+import { badRequest, ok, serverError } from "../helpers/http.helper"
 import { Controller } from "../protocols/controllers"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignupController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', "username", 'email', 'password', 'driver_license']
+    try {
+      const requiredFields = ['name', "username", 'email', 'password', 'driver_license']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamsError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamsError(field))
+        }
       }
-    }
-    const account = await httpRequest.body
+      const account = await httpRequest.body
 
-    return ok(account)
+      return ok(account)
+    } catch (error) {
+      serverError(error)
+    }
   }
 }
