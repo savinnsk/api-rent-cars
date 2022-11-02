a65ad6fa326155ac07e419b7c06d6ea2df2a932c
Savio Picanço <saviopbrito@outlook.com>
2022-11-02 10:53:52 -0300

    config: add mongodb-jest

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  preset: "@shelf/jest-mongodb",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
