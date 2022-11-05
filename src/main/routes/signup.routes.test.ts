2775c131091ff39e808294473a3432851b1943ca
Savio Picanço <saviopbrito@outlook.com>
2022-11-05 17:29:11 -0300

    test: add Should return an account on success

---
+++ src/main/routes/signup.routes.test.ts
@@ -0,0 +1,33 @@
+
+import app from '../config/app'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/helpers/mongo-helper'
+
+describe("Signup Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'savio',
+        username: "savinnsk",
+        email: 'savio@mail.com',
+        password: 'savio123',
+        driver_license: '123'
+
+      })
+      .expect(200)
+  })
+})
