51c672706168399161461f8dd52b1e9315885902
Savio Picanço <saviopbrito@outlook.com>
2022-11-05 13:45:51 -0300

    test: should return false if email is invalid

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -11,11 +11,21 @@ const makeSut = (): EmailValidator => {
 }
 
 describe("Email Validator", () => {
-  test("should return a boolean if email is valid ", () => {
+  test("should return true if email is valid ", () => {
     const sut = makeSut()
     const fakeEmail = "valid_mail@mail.com"
     const response = sut.isValid(fakeEmail)
 
     expect(response).toBe(true)
+  })
+
+  test("should return false if email is invalid ", () => {
+    const sut = makeSut()
+    const fakeEmails = ["invalid_mail.com", "@mail.com", ".com", "@@@"]
+
+    fakeEmails.forEach(email => {
+      const response = sut.isValid(email)
+      expect(response).toBe(false)
+    })
   })
 })
