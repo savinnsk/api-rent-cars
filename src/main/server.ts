b5725751684368dc9515273f06ba3eb3ee0a7230
Savio Picanço <saviopbrito@outlook.com>
2022-11-05 17:10:49 -0300

    feat: add server

---
+++ src/main/server.ts
@@ -0,0 +1,8 @@
+import { MongoHelper } from "../infra/helpers/mongo-helper"
+import env from "./config/env"
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import ("./config/app")).default
+    app.listen(env.port, () => console.log(`Server is running at ${env.port}`))
+  })
