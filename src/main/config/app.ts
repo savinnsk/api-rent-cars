c090127b6d1de4dd474bf0b03ccbaff0f4094ff2
Savio Picanço <saviopbrito@outlook.com>
2022-11-12 16:03:02 -0300

    feat : add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import setupMiddlewares from "../config/middlewares"
 import setupRoutes from "../config/routes"
+import setupSwagger from "./config-swagger"
 
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: "3.0.0",
+  info: {
+    title: "api-rent-cars",
+    description: "API to rent cars",
+    version: "1.0.0"
+  }
+}
